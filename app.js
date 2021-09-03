51571628d1ae46422118ab2d9da09fd4491e5f9f
devgaur15 <devgaur15@gmail.com>
2021-09-03 19:37:18 +0530

    arithmetic operators

--- app.js
+++ app.js
@@ -2,15 +2,26 @@
 // let Name = "Dev"
 // console.log(Name)
 
-let age = 19;
+// let age = 19;
 // console.log(typeof age)
-let Name = 'hsgyugwdydgydfydgfyfd'
-let brand 
-console.log(brand)
-console.log(Name)
+// let Name = 'hsgyugwdydgydfydgfyfd'
+// let brand 
+// console.log(brand)
+// console.log(Name.toUpperCase())
+// let arr = [1,2,3,4,5]
+// console.log(arr)
+// console.log(typeof arr)
+// arr.push("15sept")
+// console.log(arr)
 
-let arr = [1,2,3,4,5]
-console.log(arr)
-console.log(typeof arr)
-arr.push("15sept")
-console.log(arr)
+// operators in JS
+// arithmetic operators
+const a=7
+const b=8
+console.log(a+b)
+console.log('-----------------------------')
+let c=0
+c++     //c=c+1
+c+=8   //c=c+8
+console.log(c)
+
