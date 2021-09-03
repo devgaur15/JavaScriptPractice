f902173a4523e87c59a029f8cc22da8ad5c34a67
devgaur15 <devgaur15@gmail.com>
2021-09-03 20:04:33 +0530

    comparison operators

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-let a=25
-a+=7
-console.log(a)
+// comparison operator
+// const a=21
+// const b=7
+// const c=21
+
+// console.log('a>b', a>b)
+// console.log('a<b', a<b)
+// console.log('a==b', a==b)
+// console.log('a==c', a==c)
+
+const a = 11
+const b= '11'
+console.log(a==b)
+console.log(a===b)
+// (===) = (==) && type
+
