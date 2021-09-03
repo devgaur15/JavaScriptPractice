72b3edf52f9f356879e1f4b5673019016ad9c749
devgaur15 <devgaur15@gmail.com>
2021-09-03 15:41:45 +0530

    data types done

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
-console.log('Hey this is my first javascript code    YAY!!')
-console.log('djhsud73838388ndnd')
-console.warn("This is a warning")
-console.error("This is an error")
+// Data Types in js
+// let Name = "Dev"
+// console.log(Name)
+
+let age = 19;
+// console.log(typeof age)
+let Name = 'hsgyugwdydgydfydgfyfd'
+let brand 
+console.log(brand)
+console.log(Name)
+
+let arr = [1,2,3,4,5]
+console.log(arr)
+console.log(typeof arr)
+arr.push("15sept")
+console.log(arr)
