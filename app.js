d6685a5f3f756371f760e4780e56ff60a913e95c
devgaur15 <devgaur15@gmail.com>
2021-09-08 01:24:09 +0530

    dark mode done

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
-const element = document.querySelector('#selectme')
-// element.innerHTML = "This change is from JS"
-// element.style.color = "cyan"
-// element.style.fontSize = "40px"
-// console.log(element.classList)
-// element.classList.add('five')
-// console.log(element.classList)
-// element.classList.remove('one')
-// console.log(element.classList)
+const button = document.querySelector('button')
+const body = document.querySelector('body')
 
-const value = element.classList.contains('three')
-console.log(value)
-
+button.addEventListener('click', () => {
+    body.classList.toggle('dark')
+})
