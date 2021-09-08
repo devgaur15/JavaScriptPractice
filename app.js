379cc2caec2f1a0f9206f6e764ea85742c637003
devgaur15 <devgaur15@gmail.com>
2021-09-08 20:49:27 +0530

    ES6 arrow function done

--- app.js
+++ app.js
@@ -1,70 +1,31 @@
 console.log("merged class 1 ES6")
+// ES6 arrow functions
+// function add(x, y) {
+//     return x+y;
+// }
+// console.log(add(10, 20));
 
-// normal array calling way
-const arr = [1,2,3,4]
-console.log(arr[2])
+// let add = function(x,y) {
+//     return x+y;
+// }
+// console.log(add(10,20))
 
-// ES6 destructuring manner
-const [one,two,three] = arr
-console.log(three)
+// ES6 arrow function
+let add = (x, y) => {
+    return x + y;
+  };
+  console.log(add(10, 20));
 
-function getScores() {
-    return [90,100]
+  const counter = {
+    count: 1,
+    nikhil: function() {
+        return ++(this.count);
+    },
+    // key: this
 }
-
-let [x,y,z] = getScores()
-console.log(x)
-console.log(y)
-console.log(z)
-// const is immutable, hence we use let/var
-
-
-// normal object destructuring
-const obj = {
-    name: "Dev Gaur",
-    email: "devgaur15",
-    age: 20
-}
-console.log(obj.email)
-
-// ES6 object destructuring
-let {name,email,age, dum} = obj;
-console.log(email)
-let person = {
-    firstname: "Dev",
-    lastname: "Gaur",
-    age1: 21,
-    middlename: "DNA"
-}
-let {age1, firstname, lastname, middlename="" } = person;
-console.log(age1);
-console.log(firstname);
-console.log(lastname);
-console.log(middlename);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(counter)
+console.log(counter.nikhil());
+console.log(counter.nikhil());
+console.log(counter.nikhil());
+console.log(counter)
+  
