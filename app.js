12e78e6f3a8d2db30d623b830b2af5840b5b9b93
devgaur15 <devgaur15@gmail.com>
2021-09-08 09:20:39 +0530

    ES6 functions

--- app.js
+++ app.js
@@ -8,14 +8,16 @@ function logName(Name = "Whats your name") {
 
 logName("Dev")
 
-function add(a=0, b=0){
-    const sum =a+b
-    return sum
+// function add(a=0, b=0){
+//     const sum =a+b
+//     return sum
+// }
+// const result = add(5,8)
+// console.log(result)
+const add = (a=0, b=0) =>{
+    return a+b
 }
-const result = add(5,8)
-console.log(result)
-
-
+console.log(add(12,13))
 // console.log(rock)
 // console.log(paper)
 // console.log(scissors)
