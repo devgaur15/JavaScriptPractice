475a79ebdb95c28cbc41d865dc33443378fd6ae5
devgaur15 <devgaur15@gmail.com>
2021-09-08 09:03:27 +0530

    keyword function

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
-const button = document.querySelector('button')
-const body = document.querySelector('body')
+// const rock = document.querySelector('#rock')
+// const paper = document.querySelector('#paper')
+// const scissors = document.querySelector('#scissors')
 
-button.addEventListener('click', () => {
-    body.classList.toggle('dark')
-})
+function logName(Name = "Whats your name") {
+    console.log(Name)
+}
+
+logName("Dev")
+
+function add(a=0, b=0){
+    const sum =a+b
+    return sum
+}
+const result = add(5,8)
+console.log(result)
+
+
+// console.log(rock)
+// console.log(paper)
+// console.log(scissors)
