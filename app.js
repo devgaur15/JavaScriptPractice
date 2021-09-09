d22a74f7396b058d89c13ed7cc099cf7a0a44e42
devgaur15 <devgaur15@gmail.com>
2021-09-09 19:50:53 +0530

    es6 filter done

--- app.js
+++ app.js
@@ -1,23 +1,16 @@
 console.log("MERGED CLASS 2 ES6");
-
-let ranks = [1, 2, 3, 4];
-
-console.log(
-  ranks.map((e, index) => {
-    return e * 100;
-  })
-);
-
-console.log(
-  ranks.forEach((e, index) => {
-    return e * 100;
-  })
-);
-
-let newarr = ranks.map((e, index) => {
-    console.log(e)
-    console.log(index)
-    if(e%2===0)
-        return e
+let ranks = [1,2,3,4,5,6]
+let newmaparr = ranks.map((e, index) => {
+  console.log(e)
+  if(e%2===0)
+      return e
 })
-console.log(newarr);
+console.log(newmaparr);
+
+
+let newfilterarr = ranks.filter((e, index) => {
+  console.log(e)
+  if(e%2===0)
+      return e
+})
+console.log(newfilterarr);
